7a8413df3efdbf62303e701b75d86a0618104eb0
Dávid Benko <david.benko@vut.cz>
2023-07-18 15:32:27 +0200

    Implement React interface
    Supports API connectivity testing and autocompletion of entities
    and attributes from the API.

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,31 +1,104 @@
-import React, { ChangeEvent } from 'react';
-import { InlineField, Input } from '@grafana/ui';
-import { QueryEditorProps } from '@grafana/data';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { Checkbox, InlineField, Input, Select } from '@grafana/ui';
+import { QueryEditorProps, SelectableValue } from '@grafana/data';
+
 import { DataSource } from '../datasource';
 import { MyDataSourceOptions, MyQuery } from '../types';
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
-export function QueryEditor({ query, onChange, onRunQuery }: Props) {
-  const onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange({ ...query, queryText: event.target.value });
-  };
+export function QueryEditor({ datasource, query, onChange, onRunQuery }: Props) {
+  const { onlyMasterRecords, entity, eid, attr } = query;
 
-  const onConstantChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange({ ...query, constant: parseFloat(event.target.value) });
-    // executes the query
+  const [entitySpec, setEntitySpec] = useState<any>({});
+  const [entities, setEntities] = useState<Array<SelectableValue<string>>>([]);
+  const [attributes, setAttributes] = useState<Array<SelectableValue<string>>>([]);
+  const [attrState, setAttrState] = useState<string | null>(attr || null);
+
+  // Load entity spec (async)
+  useEffect(() => {
+    datasource.getEntitySpec().then(resp => {
+      setEntitySpec(resp);
+
+      setEntities(Object.keys(resp).map(key => {
+        const spec = resp[key];
+        return {
+          value: key,
+          label: `${spec.name} (${key})`,
+          description: `~ ${spec.eid_estimate_count} EIDs`
+        }
+      }));
+    });
+  }, [datasource]);
+
+  // Reflect entity's attributes
+  useEffect(() => {
+    const attrs = entitySpec[entity || '']?.attribs || {};
+
+    setAttributes(Object.keys(attrs).map(key => {
+      return {
+        value: key,
+        label: `${attrs[key].name} (${key})`,
+        description: attrs[key].description
+      }
+    }));
+
+    // Reset attribute <select>
+    setAttrState(null);
+  }, [entitySpec, entity]);
+
+  // On change hooks
+  const onOnlyMasterRecordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange({ ...query, onlyMasterRecords: event.target.checked });
     onRunQuery();
   };
 
-  const { queryText, constant } = query;
+  const onEntityChange = (value: SelectableValue<string>) => {
+    delete query.attr;
+    onChange({ ...query, entity: value.value });
+    onRunQuery();
+  };
+
+  const onEidChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange({ ...query, eid: event.target.value });
+    onRunQuery();
+  };
+
+  const onAttrChange = (value: SelectableValue<string>) => {
+    setAttrState(value.value || null);
+    onChange({ ...query, attr: value.value });
+    onRunQuery();
+  };
 
   return (
     <div className="gf-form">
-      <InlineField label="Constant">
-        <Input onChange={onConstantChange} value={constant} width={8} type="number" step="0.1" />
+      <InlineField
+        label="Only master record"
+        tooltip="If checked, show only master record data (current values).
+          If unchecked, shows historic data. This is handy for tabular overviews."
+      >
+        <Checkbox value={onlyMasterRecords} onChange={onOnlyMasterRecordChange} />
       </InlineField>
-      <InlineField label="Query Text" labelWidth={16} tooltip="Not used yet">
-        <Input onChange={onQueryTextChange} value={queryText || ''} />
+      <InlineField label="Entity" required>
+        <Select
+          options={entities}
+          value={entity}
+          onChange={onEntityChange}
+          isLoading={entities.length === 0}
+          disabled={entities.length === 0}
+        />
+      </InlineField>
+      <InlineField label="Entity ID">
+        <Input onChange={onEidChange} value={eid || ''} />
+      </InlineField>
+      <InlineField label="Attribute">
+        <Select
+          options={attributes}
+          value={attrState}
+          onChange={onAttrChange}
+          isLoading={entities.length === 0}
+          disabled={attributes.length === 0}
+        />
       </InlineField>
     </div>
   );
--- src/datasource.ts
+++ src/datasource.ts
@@ -6,14 +6,49 @@ import {
   MutableDataFrame,
   FieldType,
 } from '@grafana/data';
+import { getBackendSrv } from '@grafana/runtime';
 
 import { MyQuery, MyDataSourceOptions } from './types';
 
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
+  // DP3's API URL
+  private apiUrl: string;
+
+  /**
+   * Constructs DataSource object
+   * @param instanceSettings Settings
+   */
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
+
+    this.apiUrl = instanceSettings.jsonData.apiUrl || '';
   }
 
+  /**
+   * Returns backend full API URL with path
+   * @param path Path to be prefixed with API URL prefix
+   * @return     Full URL
+   */
+  getBackendUrl(path: string): string {
+    // Prefix without trailing slash
+    const prefix = this.apiUrl.replace(/\/$/, '');
+    return prefix + path;
+  }
+
+  /**
+   * Gets entity specification
+   *
+   * Contains list of entities, their attributes (with types), eid counts,...
+   */
+  async getEntitySpec() {
+    return await getBackendSrv().get(this.getBackendUrl('/entities'))
+  }
+
+  /**
+   * Queries DP3's API for data
+   * @param  options Query options
+   * @return         Data
+   */
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
     const { range } = options;
     const from = range!.from.valueOf();
@@ -25,7 +60,6 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
         refId: target.refId,
         fields: [
           { name: 'Time', values: [from, to], type: FieldType.time },
-          { name: 'Value', values: [target.constant, target.constant], type: FieldType.number },
         ],
       });
     });
@@ -33,11 +67,23 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     return { data };
   }
 
+  /**
+   * Health-checks DP3's API
+   */
   async testDatasource() {
-    // Implement a health check for your data source.
+    const successMsg = 'It works!';
+
+    // Health check
+    const result = await getBackendSrv().get(this.getBackendUrl('/'));
+    const success = result.detail === successMsg;
+
+    if (success) {
+      return { status: 'success', message: successMsg };
+    }
+
     return {
-      status: 'success',
-      message: 'Success',
+      status: 'error',
+      message: `Unexpected body: ${JSON.stringify(result)}`,
     };
   }
 }
