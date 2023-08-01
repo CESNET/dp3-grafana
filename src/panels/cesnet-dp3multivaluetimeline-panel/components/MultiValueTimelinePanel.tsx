77b1ba9566ca0b8ec005793b79d82ddb7a072262
Dávid Benko <david.benko@vut.cz>
2023-08-01 10:54:03 +0200

    Push initial implementation of multi-value timeline based on uPlot
    This utilizes some of Grafana's internal APIs which may (and will)
    change in the future. It's not stable nor documented properly, so
    this commit is only for future reference and it's going to be
    reimplemented using other plot library. Further implementation
    over existing internal Grafana's APIs would mean
    reverse-engineering everything from built-in panel plugins and
    creating a lot of dirty hacks to overcome the limits. I still
    wanted to try it, because other libraries and not great to work
    with anyway, but it's not reasonable anymore.
    
    Reimplementation in other library comes with drawbacks: Grafana
    theme is not going to be fully respected and we will have to
    create our own bindings to various events (in React, etc.).

--- src/panels/cesnet-dp3multivaluetimeline-panel/components/MultiValueTimelinePanel.tsx
+++ src/panels/cesnet-dp3multivaluetimeline-panel/components/MultiValueTimelinePanel.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+
 import { PanelProps } from '@grafana/data';
-import { TimeSeries, TooltipPlugin, TooltipDisplayMode, ZoomPlugin } from '@grafana/ui';
-import { LegendDisplayMode, VizLegendOptions } from '@grafana/schema';
+import { FIXED_UNIT, UPlotChart, UPlotConfigBuilder, useTheme2 } from '@grafana/ui';
+import { AxisPlacement, ScaleDirection, ScaleOrientation } from '@grafana/schema';
 
 import { SimpleOptions } from '../types';
 
@@ -17,38 +18,135 @@ export function MultiValueTimelinePanel({
   onChangeTimeRange,
   replaceVariables,
 }: Props) {
-  // Hide legend
-  const legendConfig: VizLegendOptions = {
-    calcs: [],
-    displayMode: LegendDisplayMode.Hidden,
-    placement: 'bottom',
-    showLegend: false
-  };
+  const theme = useTheme2();
+
+  // Check length of data
+  if (data.series.length === 0) { 
+    return (
+      <div className="panel-empty">
+        <p>No data</p>
+      </div>
+    );
+  }
+
+  if (data.series.length > 1) { 
+    return (
+      <div className="panel-empty">
+        <p>Only one serie is allowed</p>
+      </div>
+    );
+  }
+
+  // Get the frame
+  const frame = data.series[0];
+  const correctUsageMsg = "Are you fetching multi-value observations history from DP3?";
+
+  console.log(frame);
+
+  // Expect 2 or 3 fields in total
+  if (frame.fields.length < 2 || frame.fields.length > 3) { 
+    return (
+      <div className="panel-empty">
+        <p>
+          Expected 2 or 3 fields in total, got {frame.fields.length}.
+          <br />
+          {correctUsageMsg}
+        </p>
+      </div>
+    );
+  }
+
+  const timeField = frame.fields[0];
+  const dataField = frame.fields[1];
+  const confidenceField = frame.fields[2];
+
+  // Expect frame fields `t`, `XXX` (and optionally `XXX#c`)
+  if (timeField.name !== 't') { 
+    return (
+      <div className="panel-empty">
+        <p>
+          First field must be named <code>t</code>.
+          <br />
+          {correctUsageMsg}
+        </p>
+      </div>
+    );
+  }
+
+  if (confidenceField && dataField.name + '#c' !== confidenceField.name) {
+    return (
+      <div className="panel-empty">
+        <p>
+          Expected field <code>{dataField.name}#c</code> after field
+          <code>{dataField.name}</code>, got
+          <code>{confidenceField.name}</code>.
+          <br />
+          {correctUsageMsg}
+        </p>
+      </div>
+    );
+  }
+
+  const builder = new UPlotConfigBuilder(timeZone);
+
+  //const xScaleUnit = 'time';
+  const xScaleKey = 'x';
+
+  builder.addScale({
+    scaleKey: xScaleKey,
+    isTime: true,
+    orientation: ScaleOrientation.Horizontal,
+    direction: ScaleDirection.Right,
+    range: [timeRange.from.valueOf(), timeRange.to.valueOf()],
+  });
+
+  // builder.addScale({
+  //   scaleKey: FIXED_UNIT, // y
+  //   isTime: false,
+  //   orientation: ScaleOrientation.Vertical,
+  //   direction: ScaleDirection.Up,
+  //   //range: ...
+  // });
+
+  builder.addAxis({
+    scaleKey: xScaleKey,
+    isTime: true,
+    //splits: false,
+    placement: AxisPlacement.Bottom,
+    timeZone: timeZone,
+    theme,
+    grid: { show: true },
+  });
+
+  // builder.addAxis({
+  //   scaleKey: FIXED_UNIT, // y
+  //   isTime: false,
+  //   placement: AxisPlacement.Left,
+  //   //splits: coreConfig.ySplits,
+  //   //values: coreConfig.yValues,
+  //   grid: { show: false },
+  //   ticks: { show: false },
+  //   gap: 16,
+  //   theme,
+  // });
+
+  let distinctDataValues = [...new Set(dataField.values.toArray())];
+
+  for (const _ of distinctDataValues) {
+    builder.addSeries({
+      scaleKey: FIXED_UNIT,
+      theme,
+    });
+  }
 
   return (
-    <div>
-      <TimeSeries
-        width={width}
-        height={height}
-        timeRange={timeRange}
-        timeZone={timeZone}
-        frames={data.series}
-        legend={legendConfig}
-      >
-        {(config, alignedDataFrame) => {
-          return (
-            <>
-              <TooltipPlugin
-                config={config}
-                data={alignedDataFrame}
-                mode={TooltipDisplayMode.Multi}
-                timeZone={timeZone}
-              />
-              <ZoomPlugin config={config} onZoom={onChangeTimeRange} />
-            </>
-          );
-        }}
-      </TimeSeries>
-    </div>
+    <UPlotChart
+      width={width}
+      height={height}
+      data={[]}
+      config={builder}
+      timeRange={timeRange}
+    >
+    </UPlotChart>
   );
 }
