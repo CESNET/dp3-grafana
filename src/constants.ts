0a82c1e262c61d6e63c697dbb313dbd532e772bd
Dávid Benko <david.benko@vut.cz>
2023-08-09 17:20:40 +0200

    Add constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,13 @@
 import pluginJson from './plugin.json';
 
+/**
+ * Base URL of this plugin
+ * @type {String}
+ */
 export const PLUGIN_BASE_URL = `/a/${pluginJson.id}`;
+
+/**
+ * ID of datasource plugin for DP3
+ * @type {String}
+ */
+export const PLUGIN_DATASOURCE_ID = 'cesnet-dp3-datasource';
